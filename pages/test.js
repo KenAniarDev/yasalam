aff1c94ea0d3368b1061d30f1891da57d1427013
Nestor Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-02-11 16:24:03 +0800

    added table

--- pages/test.js
+++ pages/test.js
@@ -1,14 +1,13 @@
 import { useState } from 'react';
 import styles from '../styles/Home.module.css';
-import ReactQuill from '../components/ReactQuill';
+import SampleTable from '../components/SampleTable';
 
 export default function Home() {
   const [items, setItems] = useState('');
 
   return (
     <div className={styles.container}>
-      <button onClick={() => console.log(items)}>console.log</button>
-      <ReactQuill value={items} onChange={setItems} />
+      <SampleTable />
     </div>
   );
 }
