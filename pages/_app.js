ce2565e9f29dad9502a20c495981ffe6756a2a19
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-02-17 18:18:57 +0800

    fix appjs and lockjson

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,23 @@
-import Head from "next/head";
-import { Toaster } from "react-hot-toast";
-import "styles/App.css";
-import "styles/slick-theme.css";
-import "styles/slick.css";
-import "../styles/globals.css";
+import Head from 'next/head';
+import { Toaster } from 'react-hot-toast';
+import 'styles/App.css';
+import 'styles/slick-theme.css';
+import 'styles/slick.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-   return (
-      <>
-         <Head>
-            <link
-               rel="stylesheet"
-               type="text/css"
-               href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-            />
-         </Head>
-         <Toaster />
-         <Component {...pageProps} />
-      </>
-   );
-
+  return (
+    <>
+      <Head>
+        <link
+          rel='stylesheet'
+          type='text/css'
+          href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+        />
+      </Head>
+      <Toaster />
+      <Component {...pageProps} />
+    </>
+  );
+}
 export default MyApp;
