b6b195af1db9cb65f7554169302c267b23ae3d6d
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-02-24 13:18:27 +0800

    try fixing update with referral

--- pages/api/payment/update-with-referral.js
+++ pages/api/payment/update-with-referral.js
@@ -1,43 +1,5 @@
-import {
-  getMemberByEmail,
-  memberPaid,
-  checkIfReferralExist,
-  deleteReferral,
-} from '../../../utils/firebaseAdmin';
-import moment from 'moment';
+import { updateWithReferral } from '../../../utils/firebaseAdmin';
 
 export default async function handler(req, res) {
-  const date = new Date();
-  console.log(req.body.code);
-  try {
-    const isReferral = await checkIfReferralExist(req.body.code);
-
-    if (!isReferral) {
-      return res.status(400).send('Invalid referral');
-    }
-    const member = await getMemberByEmail(req.body.email);
-    if (!member) {
-      return res.status(400).send('User not found');
-    }
-
-    if (member.isPaid) {
-      return res.status(400).send('User already paid');
-    }
-
-    const issueDate = moment(date).format('YYYY-MM-DD');
-    const expiryDate = moment(date).add(1, 'years').format('YYYY-MM-DD');
-
-    await memberPaid(member.email, {
-      isPaid: true,
-      issueDate: issueDate,
-      expiryDate: expiryDate,
-    });
-
-    await deleteReferral(req.body.code);
-
-    return res.status(200).send('Activated');
-  } catch (error) {
-    console.log(error);
-    return res.status(400).send('Error Please Try Again');
-  }
+  updateWithReferral(req, res);
 }
--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -847,3 +847,38 @@ export const deleteFavorite = async (req, res) => {
     res.status(400).send('Server Error. Please try again');
   }
 };
+export const updateWithReferral = async (req, res) => {
+  const date = new Date();
+  console.log(req.body.code);
+  try {
+    const isReferral = await checkIfReferralExist(req.body.code);
+
+    if (!isReferral) {
+      return res.status(400).send('Invalid referral');
+    }
+    const member = await getMemberByEmail(req.body.email);
+    if (!member) {
+      return res.status(400).send('User not found');
+    }
+
+    if (member.isPaid) {
+      return res.status(400).send('User already paid');
+    }
+
+    const issueDate = moment(date).format('YYYY-MM-DD');
+    const expiryDate = moment(date).add(1, 'years').format('YYYY-MM-DD');
+
+    await memberPaid(member.email, {
+      isPaid: true,
+      issueDate: issueDate,
+      expiryDate: expiryDate,
+    });
+
+    await deleteReferral(req.body.code);
+
+    return res.status(200).send('Activated');
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send('Error Please Try Again');
+  }
+};
